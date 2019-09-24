3007108208c22fda13d022d5aa5926d2e54ef695
dothachthong <dothachthong@gmail.com>
2019-09-24 12:42:31 +0700

    commit

---
+++ 1299/1299-content.php
@@ -0,0 +1,28 @@
+<?php
+$url_host = 'http://'.$_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-1299">
+    <div class="space" style="margin-top: 40px;"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="product-info">
+                    <h1>Your favourite shoes on sale</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        <br>Quisque ultrices justo sed vehicula fringilla.</p>
+                        <a href="#" class="theme-btn-1">Buy now</a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                <img src="<?php echo $url_path ?>/images/sale-product-2.png" class="img-responsive" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
